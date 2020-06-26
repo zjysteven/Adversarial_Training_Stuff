a5c1e59e9cf4fd238fda30ca8ffe1aac78baf336
Jingyang Zhang <jz288@hl279-cmp-04.egr.duke.edu>
2020-06-26 18:33:16 -0400

    Add models folder

--- utils.py
+++ utils.py
@@ -16,36 +16,6 @@ from torchvision.datasets import CIFAR10
 from advertorch.utils import NormalizeByChannelMeanStd
 from models import WideResNet
 
-"""
-###################################
-# Normalizer                      #
-###################################
-class NormalizeByChannelMeanStd(nn.Module):
-    def __init__(self, mean, std):
-        super(NormalizeByChannelMeanStd, self).__init__()
-        if not isinstance(mean, torch.Tensor):
-            mean = torch.tensor(mean)
-        if not isinstance(std, torch.Tensor):
-            std = torch.tensor(std)
-        self.register_buffer("mean", mean)
-        self.register_buffer("std", std)
-
-    def forward(self, tensor):
-        return normalize_fn(tensor, self.mean, self.std)
-
-    def extra_repr(self):
-        return 'mean={}, std={}'.format(self.mean, self.std)
-
-
-def normalize_fn(tensor, mean, std):
-    '''Differentiable version of torchvision.functional.normalize'''
-    # here we assume the color channel is in at dim=1
-    mean = mean[None, :, None, None].to(tensor.device)
-    std = std[None, :, None, None].to(tensor.device)
-    return tensor.sub(mean).div(std)
-"""
-
-
 
 ###################################
 # Models                          #
