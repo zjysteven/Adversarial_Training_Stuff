af41fe24825f8cf6c0e95cb38c74ef1c954f62e8
Steven Zhang <jz288@hl279-cmp-01.egr.duke.edu>
2020-07-13 22:07:48 -0400

    Delete eval-when-attack argument

--- scripts/run_cat.sh
+++ scripts/run_cat.sh
@@ -7,7 +7,7 @@ cd $DIR
 GPU="4,5,6,7"
 
 python train_cat.py --gpu 4,5,6,7 --epochs 200 --lr-sch multistep --sch-intervals 80 140 180 \
-    --seed 233 --batch-size 256 --amp --eval-when-attack
+    --seed 233 --batch-size 256 --amp
 
 python train_cat.py --gpu 4,5,6,7 --epochs 200 --lr-sch multistep --sch-intervals 80 140 180 \
-    --seed 233 --batch-size 256 --amp --eval-when-attack --depth 34
+    --seed 233 --batch-size 256 --amp --depth 34
