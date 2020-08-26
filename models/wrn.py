c975a74096e02f0dc7b4c453f26c1284195d7454
Steven Zhang <jz288@hl279-cmp-01.egr.duke.edu>
2020-08-26 18:55:43 -0400

    Update the final avepool of wrn for flexibility

--- models/wrn.py
+++ models/wrn.py
@@ -86,6 +86,7 @@ class WideResNet(nn.Module):
         out = self.block2(out)
         out = self.block3(out)
         out = self.relu(self.bn1(out))
-        out = F.avg_pool2d(out, 8)
+        assert out.shape[-2] == out.shape[-1]
+        out = F.avg_pool2d(out, out.shape[-1])
         out = out.view(-1, self.nChannels)
         return self.fc(out)
